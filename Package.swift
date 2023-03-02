5584ee7c205237c14267a52448047acbef934e26
marcprux <mwp1@cornell.edu>
2023-03-02 15:49:23 -0500

    Release 0.0.18

--- Package.swift
+++ Package.swift
@@ -19,11 +19,11 @@ let package = Package(
                     .writeToPackageDirectory(reason: "skip creates source files"),
                     //.allowNetworkConnections(scope: .all(ports: []), reason: "skip uses gradle to download dependendies from the network"), // awaiting Swift 5.9
                 ]),
-            dependencies: ["SkipCLI"]),
+            dependencies: ["skiptool"]),
         .executableTarget(name: "SkipCLI", plugins: ["skiptool"]),
         .testTarget(name: "SkipCLITests", dependencies: ["SkipCLI"]),
     ]
 )
 
-package.targets += [.binaryTarget(name: "skiptool", url: "https://github.com/skipsource/skip/releases/download/0.0.17/skiptool.artifactbundle.zip", checksum: "e6acd47ffa14ee1df3c555c6b5b3fc6ea5b8f79f6b326e4d52b417d4083becc5")]
+package.targets += [.binaryTarget(name: "skiptool", url: "https://github.com/skipsource/skip/releases/download/0.0.18/skiptool.artifactbundle.zip", checksum: "f9ab0208f3d743b3f4da3f5922f2618f43ea53ad40da1d7161be4bc399936673")]
 
