cd26b8b19f11cf314f6dfee2e3b00c84f2504848
marcprux <mwp1@cornell.edu>
2023-03-04 10:02:10 -0500

    Release 0.0.39

--- Package.swift
+++ Package.swift
@@ -6,7 +6,13 @@ let package = Package(
     products: [
         .plugin(name: "skip", targets: ["SkipCommandPlugIn"]),
         .plugin(name: "skippy", targets: ["SkipBuildPlugIn"]),
-        .library(name: "SkipUnitTestSupport", targets: ["SkipUnitTestSupport"])
+        .library(name: "SkipUnitTestSupport", targets: ["SkipUnitTestSupport"]),
+        //.library(name: "SkipKit", targets: ["SkipKit"]),
+    ],
+    dependencies: [
+        //.package(url: "https://github.com/skiptools/cross-foundation.git", from: "0.0.0"),
+        //.package(url: "https://github.com/skiptools/cross-ui.git", from: "0.0.0"),
+        //.package(url: "https://github.com/skiptools/cross-test.git", from: "0.0.0"),
     ],
     targets: [
         .target(name: "SkipUnitTestSupport"),
@@ -22,8 +28,13 @@ let package = Package(
         .plugin(name: "SkipBuildPlugIn",
             capability: .buildTool(),
             dependencies: ["skiptool"]),
+        //.target(name: "SkipKit", dependencies: [
+        //    .product(name: "CrossFoundation", package: "cross-foundation"),
+        //    .product(name: "CrossUI", package: "cross-ui"),
+        //    .product(name: "CrossTest", package: "cross-test"),
+        //]),
     ]
 )
 
-package.targets += [.binaryTarget(name: "skiptool", url: "https://github.com/skiptools/skip/releases/download/0.0.38/skiptool.artifactbundle.zip", checksum: "38d2da61d3770eaa55e0619b442cbe2fef05db4bbc9057238cc48f210d1c644e")]
+package.targets += [.binaryTarget(name: "skiptool", url: "https://github.com/skiptools/skip/releases/download/0.0.39/skiptool.artifactbundle.zip", checksum: "872f6505c76ccdb320968e8a4a9a4626a5b9ee86ca561dd436a2cc88a4f51a05")]
 
--- Sources/SkipUnitTestSupport/SkipUnitTestSupport.swift
+++ Sources/SkipUnitTestSupport/SkipUnitTestSupport.swift
@@ -1,8 +1,1 @@
-//
-//  File.swift
-//  
-//
-//  Created by Marc Prud'hommeaux on 3/3/23.
-//
-
 import Foundation
