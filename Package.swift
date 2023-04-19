d7beccb723833e541328cf7ccd800be69c44916e
marcprux <mwp1@cornell.edu>
2023-04-19 08:08:47 -0400

    Release 0.3.46

--- Package.swift
+++ Package.swift
@@ -75,5 +75,5 @@ if let localPath = ProcessInfo.processInfo.environment["SKIPLOCAL"] {
     package.targets += [.executableTarget(name: "skiptool", dependencies: [.product(name: "SkipBuild", package: "SkipSource")], path: "Sources/SkipTool", sources: ["skiptool.swift"])]
 } else {
     // default to using the latest binary skiptool release
-    package.targets += [.binaryTarget(name: "skiptool", url: "https://github.com/skiptools/skip/releases/download/0.3.45/skiptool.artifactbundle.zip", checksum: "f85d172cc801b2b2a1034615ee9e265150b565baa73832601906fabfdc959b64")]
+    package.targets += [.binaryTarget(name: "skiptool", url: "https://github.com/skiptools/skip/releases/download/0.3.46/skiptool.artifactbundle.zip", checksum: "e623f63194bae638ced78b5c38f7a0b636cb96e96eb8206a9295da1497f6c833")]
 }
