d9e076ce025dd92661e726314b257161204a1209
marcprux <mwp1@cornell.edu>
2023-08-15 20:11:31 -0400

    Release 0.5.43

--- Package.swift
+++ Package.swift
@@ -86,5 +86,5 @@ if let localPath = ProcessInfo.processInfo.environment["SKIPLOCAL"] {
     package.targets += [.executableTarget(name: "skipstone", dependencies: [.product(name: "SkipBuild", package: "skipstone")])]
 } else {
     // default to using the latest binary skipstone release
-    package.targets += [.binaryTarget(name: "skipstone", url: "https://skip.tools/skiptools/skip/releases/download/0.5.42/skipstone.plugin.zip", checksum: "20a0a86bca818d91f70819284025ab730d32200c25866bc15fde018855864d0e")]
+    package.targets += [.binaryTarget(name: "skipstone", url: "https://skip.tools/skiptools/skip/releases/download/0.5.43/skipstone.plugin.zip", checksum: "a6dcb7d692c703566d7d903a1fa45af974f82be0be380751799e73b658736709")]
 }
