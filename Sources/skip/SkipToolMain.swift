271927eeda729f1187cb0044a3193f5b8db4e802
marcprux <mwp1@cornell.edu>
2023-09-03 16:52:21 -0400

    Add SkipToolMain

--- Sources/skip/SkipToolMain.swift
+++ Sources/skip/SkipToolMain.swift
@@ -3,6 +3,9 @@
 // This is free software: you can redistribute and/or modify it
 // under the terms of the GNU Lesser General Public License 3.0
 // as published by the Free Software Foundation https://fsf.org
+#if !canImport(SkipBuild)
+#error("Should only import SkipBuild for SKIPLOCAL")
+#else
 import SkipBuild
 
 /// Command-line `skip` tool.
@@ -11,5 +14,4 @@ import SkipBuild
         await SkipRunnerExecutor.main()
     }
 }
-
-
+#endif
