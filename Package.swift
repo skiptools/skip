eef74c34db7c9e1a87f63ea1f7188674489c684e
marcprux <mwp1@cornell.edu>
2023-09-22 00:23:29 -0400

    Release 0.6.73

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .target(name: "SkipTest", dependencies: [.target(name: "SkipDrive", condition: .when(platforms: [.macOS]))]),
         .testTarget(name: "SkipTestTests", dependencies: ["SkipTest"]),
         .testTarget(name: "SkipDriveTests", dependencies: ["SkipDrive"]),
-        .binaryTarget(name: "skip", url: "https://source.skip.tools/skip/releases/download/0.6.72/skip.zip", checksum: "7538cd513453760f86a72f1043551a22fdc515360785ee3e6cfdb317871bd90c")
+        .binaryTarget(name: "skip", url: "https://source.skip.tools/skip/releases/download/0.6.73/skip.zip", checksum: "3e08e04036c715150b4f873b56bed40f538a820ef0a1a157d82f177b0f9720be")
     ]
 )
 
--- Sources/SkipDrive/Version.swift
+++ Sources/SkipDrive/Version.swift
@@ -1,4 +1,4 @@
 // Copyright 2023 Skip
 
 /// The current version of SkipDrive.
-public let skipVersion = "0.6.72"
+public let skipVersion = "0.6.73"
