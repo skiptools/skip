0eb4d6c0f3de34009cef54af142292585381002a
marcprux <mwp1@cornell.edu>
2023-10-04 09:02:52 -0400

    Release 0.6.85

--- Tests/SkipDriveTests/SkipDriveTests.swift
+++ Tests/SkipDriveTests/SkipDriveTests.swift
@@ -10,10 +10,10 @@ class SkipCommandTests : XCTestCase {
             versionOut.removeLast(" (debug)".count)
         }
 
-        //XCTAssertEqual("Skip version \(SkipDrive.skipVersion)", versionOut)
+        XCTAssertEqual("Skip version \(SkipDrive.skipVersion)", versionOut)
     }
 
-    func NOtestSkipVersionJSON() async throws {
+    func testSkipVersionJSON() async throws {
         try await XCTAssertEqualAsync(SkipDrive.skipVersion, skip("version", "--json").parseJSONObject()["version"] as? String)
     }
 
