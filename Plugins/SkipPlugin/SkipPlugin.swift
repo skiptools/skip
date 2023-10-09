fb94f1f45a7a37545602e9f5c6c84fc4809289e4
marcprux <mwp1@cornell.edu>
2023-10-09 17:04:53 -0400

    Add .docc extension to input files to avoid inclusion in build outputs

--- Plugins/SkipPlugin/SkipPlugin.swift
+++ Plugins/SkipPlugin/SkipPlugin.swift
@@ -23,10 +23,11 @@ import PackagePlugin
     //let skipcodeExtension = ".skipcode.json"
 
     /// The skip transpile marker that is always output regardless of whether the transpile was successful or not
-    let skipbuildMarkerExtension = ".skipbuild"
+    /// Needs to have the extension .docc to prevent including the file in the output bundle
+    let skipbuildMarkerExtension = ".skipbuild.docc"
 
     /// The extension to add to the skippy output; these have the `docc` extension merely because that is the only extension of generated files that is not copied as a resource when a package is built: https://github.com/apple/swift-package-manager/blob/0147f7122a2c66eef55dcf17a0e4812320d5c7e6/Sources/PackageLoading/TargetSourcesBuilder.swift#L665
-    let skippyOuptputExtension = ".skippy"
+    let skippyOuptputExtension = ".skippy.docc"
 
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
         if skipRootTargetNames.contains(target.name) {
