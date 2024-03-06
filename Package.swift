94949607843a4cd7325affdd5aac2b4a1ba3b7fd
marcprux <mwp1@cornell.edu>
2024-03-06 13:45:45 -0500

    Release 0.8.21

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         .target(name: "SkipTest", dependencies: [.target(name: "SkipDrive", condition: .when(platforms: [.macOS]))]),
         .testTarget(name: "SkipTestTests", dependencies: ["SkipTest"]),
         .testTarget(name: "SkipDriveTests", dependencies: ["SkipDrive"]),
-        .binaryTarget(name: "skip", url: "https://source.skip.tools/skip/releases/download/0.8.20/skip.zip", checksum: "28012c2f3500c812e22d445d297d063affafd1773913fe5c90620162d03473e9")
+        .binaryTarget(name: "skip", url: "https://source.skip.tools/skip/releases/download/0.8.21/skip.zip", checksum: "885673fee41b46088bb590d57452d1a6210cab138ee9300b33f7864940af6b22")
     ]
 )
 
