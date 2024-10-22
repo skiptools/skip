b6a1b211d1224f4d7feb2bea3469d7573f426784
Abe White <abewhite@mac.com>
2024-10-21 23:06:30 -0500

    Release 1.1.14

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         .target(name: "SkipTest", dependencies: [.target(name: "SkipDrive", condition: .when(platforms: [.macOS]))]),
         .testTarget(name: "SkipTestTests", dependencies: ["SkipTest"]),
         .testTarget(name: "SkipDriveTests", dependencies: ["SkipDrive"]),
-        .binaryTarget(name: "skip", url: "https://source.skip.tools/skip/releases/download/1.1.13/skip.zip", checksum: "822e84b30e0cce05163f86ea2adc9a227b0fa6a2e29444500f4c1929ac67b463")
+        .binaryTarget(name: "skip", url: "https://source.skip.tools/skip/releases/download/1.1.14/skip.zip", checksum: "e4d5917dbe2be1d0d0f1c7a412c9194f5b79c17b3b2d40e05e86c300ae99ef24")
     ]
 )
 
