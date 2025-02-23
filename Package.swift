3ac8b560799089402b40b63ac7f694fd08c7153f
Abe White <abewhite@mac.com>
2025-02-22 21:55:21 -0600

    Release 1.2.32

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         .target(name: "SkipTest", dependencies: [.target(name: "SkipDrive", condition: .when(platforms: [.macOS]))]),
         .testTarget(name: "SkipTestTests", dependencies: ["SkipTest"]),
         .testTarget(name: "SkipDriveTests", dependencies: ["SkipDrive"]),
-        .binaryTarget(name: "skip", url: "https://source.skip.tools/skip/releases/download/1.2.31/skip.zip", checksum: "f5d1ea7b1beeae11005c9dee876441783a8350a7dda76aa092d1c7aee0975a3a")
+        .binaryTarget(name: "skip", url: "https://source.skip.tools/skip/releases/download/1.2.32/skip.zip", checksum: "48be3f590594a290f299b4e32cc9a525ae419772f1bdbfab31c12dc306731a67")
     ]
 )
 
--- README.md
+++ README.md
@@ -127,7 +127,7 @@ let package = Package(
         .library(name: "ModuleName", targets: ["ModuleName"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "1.2.31"),
+        .package(url: "https://source.skip.tools/skip.git", from: "1.2.32"),
         .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.0.0"),
     ],
     targets: [
